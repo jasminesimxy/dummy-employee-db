dd9ed011e74b059b5dce207f2c8b4fc475008e72
jasmine <jasminesimxy@gmail.com>
2023-02-19 15:27:30 +1100

    fixed seeds.sql error 1 but found another error...

--- main/db/seeds.sql
+++ main/db/seeds.sql
@@ -1,12 +1,11 @@
 INSERT INTO department (department_name)
 VALUES ("Engineering"),
        ("Marketing"),
-       ("Finance"),
-       ("Project Management");
+       ("Finance");
 
 
 
-INSERT INTO job_role (tile, salary, department_id)
+INSERT INTO job_role (title, salary, department_id)
 VALUES ("Software Engineer" , "140000" , 1),
        ("Data Engineer" , "100000" , 1),
        ("Systems Engineer" , "150000" , 1),
@@ -14,21 +13,21 @@ VALUES ("Software Engineer" , "140000" , 1),
        ("Marketing Manager", "80000", 2),
        ("Junior Marketing", "60000",2),
        ("Marketing Coordinator", "55000",2),
-       ("Media and Communications Coordinator", "70000",2),
+       ("Media Coordinator", "70000",2),
        ("Chief Finance Officer", "150000",3),
-       ("Procurement and Contracts Officer", "950000",3),
+       ("Procurement Officer", "950000",3),
        ("Accounts Payable Officer", "750000",3);
 
 
 INSERT INTO employee (first_name,last_name,role_id,manager_id)
-VALUES ("Donald", "O'Connell", "11" , "124" , 1),
-       ("Jennifer", "Whalen", "10" , "605" , 2),
-       ("Neena", "Marvis", "9" , "181" , 1),
-       ("Steven", "King", "8" , "908" , 3),
-       ("Neena", "Ladwig", "7" , "665" , 1),
-       ("Lex", "Fay", "6" , "452" , 2),
-       ("Bruce", "Himuro", "5" , "198" , 3),
-       ("Valli", "Vollman", "4" , "374" , 3),
-       ("Daniel", "Nayer", "3" , "458" , 1),
-       ("Laura", "Atkinson", "2" , "802" , 2),
-       ("Stephen", "Marlow", "1" , "514" , 2);
+VALUES ("Donald", "O'Connell", 1 , 1),
+       ("Jennifer", "Whalen", 1 , 2),
+       ("Neena", "Marvis", 1 , 3),
+       ("Steven", "King", 1 ,  4),
+       ("Neena", "Ladwig", 2 ,5),
+       ("Lex", "Fay", 2 , 6),
+       ("Bruce", "Himuro", 2 , 7),
+       ("Valli", "Vollman", 2 ,8),
+       ("Daniel", "Nayer", 3 , 9),
+       ("Laura", "Atkinson",3 , 10),
+       ("Stephen", "Marlow", 3, 11);
