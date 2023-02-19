e7e9402cc69f5e1fbe3c70063694799ebdac89a6
jasmine <jasminesimxy@gmail.com>
2023-02-19 12:08:43 +1100

    added schema.sql and necessary files for the project, next I need to add codes for query and seeds . sql and add node.modules

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: '',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+  );
+
+
+  //view departments,roles,employees
+
+  app.get('/api/movies', (req, res) => {
+    const sql = `SELECT id, movie_name AS title FROM movies`;
+    
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+         return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
