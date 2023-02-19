951e22b84384edc565e1fb079125002b0f9db607
jasmine <jasminesimxy@gmail.com>
2023-02-19 12:45:25 +1100

    added the code for add, view, update information onto server.js - need help for correcting my codes, not sure if it is right, awaiting for tutor to confirm/assist me. But while i wait, i will work on the prompt questions next

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ const db = mysql.createConnection(
   );
 
 
-  //view departments,roles,employees
+//view departments
 
-  app.get('/api/movies', (req, res) => {
-    const sql = `SELECT id, movie_name AS title FROM movies`;
+  app.get('??', (req, res) => {
+    const sql = `SELECT id, department_name AS "??" FROM department`;
     
     db.query(sql, (err, rows) => {
       if (err) {
@@ -39,5 +39,117 @@ const db = mysql.createConnection(
         message: 'success',
         data: rows
       });
+    });
+  });
+
+//view roles
+
+  app.get('??', (req, res) => {
+    const sql = `SELECT id, title, salary, department_id AS "??" FROM job_role`;
+    
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+         return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+  //view employees
+  app.get('', (req, res) => {
+    const sql = `SELECT employee ,first_name, last_name, role_id , manager_id AS "??" FROM employee`;
+    
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+         return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+
+
+
+// Add a department
+app.post('/??', ({ body }, res) => {
+    const sql = `INSERT INTO department (id,department_name)
+      VALUES (? ,? )`;
+    const params = [body.department_name];
+    
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
+    });
+  });
+  
+ // Add a job_role
+ app.post('/??', ({ body }, res) => {
+    const sql = `INSERT INTO job_role (id, title, salary, department_id)
+      VALUES (? , ? , ? , ? )`;
+    const params = [body.id, body.title, body.salary, body,department_id];
+    
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
+    });
+  });
+
+// Add an employee
+app.post('/??', ({ body }, res) => {
+    const sql = `INSERT INTO employee (employee, first_name, last_name, role_id, manager_id)
+      VALUES (?, ? , ? ,? ,?)`;
+    const params = [body.employee, body.first_nam, body.last_name, body.role_id, body.manager_id];
+    
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
+    });
+  });
+
+// update employees's database 
+app.put(' ?? ', (req, res) => {
+    const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
+    const params = [req.body.review, req.params.id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Movie not found'
+        });
+      } else {
+        res.json({
+          message: 'success',
+          data: req.body,
+          changes: result.affectedRows
+        });
+      }
     });
   });
